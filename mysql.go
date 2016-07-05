b263fb28d124def133d3e3588b675f4806ad778f
sakshigeminisys <sakshi@gemini-systems.net>
2016-07-05 16:04:42 -0400

    adding mustache support

--- mysql.go
+++ mysql.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-        "log"
-        "fmt"
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
-	"bytes"
+	//"bytes"
+	"encoding/json"
+	"github.com/hoisie/mustache"
 	"io/ioutil"
-        "encoding/json"
 )
 
 func DecodeJsonMysql(param json.RawMessage) Mysql {
@@ -19,93 +20,68 @@ func DecodeJsonMysql(param json.RawMessage) Mysql {
 	return s
 }
 
-func ReplaceString(fileName string, sText string, rText string) error {
-	input, err := ioutil.ReadFile(fileName)
-        if err != nil {
-        	fmt.Println(err)
-		return err
-         }
-	output := bytes.Replace(input, []byte(sText), []byte(rText), -1)
-	if err = ioutil.WriteFile(fileName, output, 0666); err != nil {
-                 fmt.Println(err)
-                 return err
-        }
+func ReplaceString(fileName string, res Mysql, MasterHost string) error {
+	fmt.Println("\nres", res)
+	context := map[string]string{
+		"HOST_IP":     MasterHost,
+		"LOGFILE":     res.LogFile,
+		"LOGPOS":      res.LogPos,
+		"REPL_USER":   res.MasterUser,
+		"REPL_PASSWD": res.MasterPass,
+		"RUSER":       res.MysqlUser,
+		"RPASSWORD":   res.MysqlPass}
+
+	output := mustache.RenderFile(fileName, context)
+	fmt.Println("testfile output", output)
+	if err := ioutil.WriteFile(fileName, []byte(output), 0666); err != nil {
+		fmt.Println(err)
+	}
 	return nil
 }
 
-
-func PopulateMysqlYaml(res Mysql, MasterHost string) error {
+func PopulateMysqlYaml(res Mysql, MasterHost string) (error, string) {
 	srcTemplate := "./templates/mysql_production_follow.yaml"
 	dstYaml := "./templates/mysql_production_follow"
 	fmt.Println("PopulateMysqlYaml:::", res)
-	/* Check if sql yaml template exists*/ 
+	/* Check if sql yaml template exists*/
 	if _, err := os.Stat(srcTemplate); err != nil {
-    		if os.IsNotExist(err) {
-        		fmt.Println("\nfile does not exist\n")
-			return err
-    		} else {
-        	 	fmt.Println("\n", err.Error())
-			return err
-    		}
-	}else{
+		if os.IsNotExist(err) {
+			fmt.Println("\nfile does not exist\n")
+			return err, ""
+		} else {
+			fmt.Println("\n", err.Error())
+			return err, ""
+		}
+	} else {
 		/* Copy the template to a new file and write values*/
 		dstYaml += "_"
-        	dstYaml += MasterHost
+		dstYaml += MasterHost
 		dstYaml += ".yaml"
 		fmt.Println("\nMasterHost:", MasterHost)
 		fmt.Println("\ndstYaml:", dstYaml)
 		if _, err := os.Stat(dstYaml); err != nil {
 			if os.IsNotExist(err) {
-  				fmt.Printf("\ncreating file\n")
+				fmt.Printf("\ncreating file\n")
 				cpCmd := exec.Command("cp", "-rf", srcTemplate, dstYaml)
-                        	err := cpCmd.Run()
-                        	if err != nil{
-                                	fmt.Printf("\nCopy command failed\n")
-                                	return err
-                        	}
+				err := cpCmd.Run()
+				if err != nil {
+					fmt.Printf("\nCopy command failed\n")
+					return err, ""
+				}
 				/*Write values to this file*/
-				err = ReplaceString(dstYaml, "HOST_IP", MasterHost)
-				if err != nil{
-                                	fmt.Printf("\nReplacing HOST_IP failed\n")
-                                	return err
-                        	}
-				err = ReplaceString(dstYaml, "LOGFILE", res.LogFile)
-                        	if err != nil{
-                                	fmt.Printf("\nReplacing LogFile failed\n")
-                                	return err
-                        	}
-				err = ReplaceString(dstYaml, "LOGPOS", res.LogPos)
-                        	if err != nil{
-                                	fmt.Printf("\nReplacing LOGPOS failed\n")
-                                	return err
-                        	}
-				err = ReplaceString(dstYaml, "REPL_USER", res.MasterUser)
-                        	if err != nil{
-                                	fmt.Printf("\nReplacing MasterUser failed\n")
-                                	return err
-                        	}
-				err = ReplaceString(dstYaml, "REPL_PASSWD", res.MasterPass)
-                        	if err != nil{
-                                	fmt.Printf("\nReplacing MasterPass failed\n")
-                                	return err
-                        	}
-				err = ReplaceString(dstYaml, "RUSER", res.MysqlUser)
-                        	if err != nil{
-                                	fmt.Printf("\nReplacing MysqlUser failed\n")
-                                	return err
-                        	}
-				err = ReplaceString(dstYaml, "RPASSWORD", res.MysqlPass)
-                        	if err != nil{
-                                	fmt.Printf("\nReplacing MysqlPass failed\n")
-                                	return err
-                        	}
-			}else{
-				return err
+				err = ReplaceString(dstYaml, res, MasterHost)
+				if err != nil {
+					fmt.Printf("\nReplacing RES failed\n")
+					return err, ""
+				}
+
+			} else {
+				return err, ""
 			}
-		}else{
+		} else {
 			fmt.Printf("\nFile already exists\n")
 		}
-	}	
+	}
 
-	return nil
+	return nil, dstYaml
 }
--- service.go
+++ service.go
@@ -1,28 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/emicklei/go-restful"
 	"log"
 	"net/http"
-	"fmt"
-	"encoding/json"
-	"github.com/emicklei/go-restful"
-	//"github.com/emicklei/go-restful/swagger"
 )
 
 type MasterYaml struct {
-        DbType string
-	MasterHost string
-        Params interface{}
+	DbType     string      `json:"dbtype"`
+	MasterHost string      `json:"masterhost"`
+	Params     interface{} `json:"params"`
 }
 
 type Mysql struct {
-        LogPos string
-        LogFile string
-	MasterUser string
- 	MasterPass string
-	MysqlUser string
-	MysqlPass string
-	
+	LogPos     string `json:"logpos"`
+	LogFile    string `json:"logfile"`
+	MasterUser string `json:"masteruser"`
+	MasterPass string `json:"masterpass"`
+	MysqlUser  string `json:"mysqluser"`
+	MysqlPass  string `json:"mysqlpass"`
 }
 
 // PUT http://localhost:8080/daemon/CreateProdFollow -d '{"DbType": "mysql", "MasterHost":"209.205.208.122"}'
@@ -32,59 +30,56 @@ func CreateProdFollow(request *restful.Request, response *restful.Response) {
 
 	var param json.RawMessage
 	JsonParam := MasterYaml{
-                Params: &param,
-        }
+		Params: &param,
+	}
 	fmt.Println("Enter CreateProdFollow function..\n ")
-        
-	if (request.HeaderParameter("Content-Type") == "application/x-www-form-urlencoded"){
+
+	if request.HeaderParameter("Content-Type") == "application/x-www-form-urlencoded" {
 
 		request.Request.Header.Set("Content-Type", "application/json")
 
 	}
-	fmt.Printf("\n\n",request.HeaderParameter("Content-Type"))
+	fmt.Printf("\n\n", request.HeaderParameter("Content-Type"))
 
 	err := request.ReadEntity(&JsonParam)
 
-	if err != nil{
-                fmt.Printf("\nfailed in readng json\n")
-                response.AddHeader("Content-Type", "text/plain")
-                response.WriteErrorString(http.StatusInternalServerError, err.Error())
-                return
-        }
+	if err != nil {
+		fmt.Printf("\nfailed in readng json\n")
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		return
+	}
 
-	
 	fmt.Println("\n\nDbType is :", JsonParam.DbType)
 
-        /* Create service for master
+	/* Create service for master
 	   TBD
 	*/
-	
-        /*Fetch DB type and decode params*/
 
- 	switch JsonParam.DbType {
-        case "mysql":
+	/*Fetch DB type and decode params*/
+
+	switch JsonParam.DbType {
+	case "mysql":
 		var res Mysql = DecodeJsonMysql(param)
-		fmt.Println("Switch ::" , res)
-		
+		fmt.Println("Switch ::", res)
+
 		/* Populate mysql template*/
-		err := PopulateMysqlYaml(res, JsonParam.MasterHost)	
-		
-		if err != nil{
+		err, yamlFile := PopulateMysqlYaml(res, JsonParam.MasterHost)
+		fmt.Println("\n\nyamlFile is :", yamlFile)
+		if err != nil {
 			fmt.Printf("\nfailed in reading SQL yaml\n")
-                	response.AddHeader("Content-Type", "text/plain")
-                	response.WriteErrorString(http.StatusInternalServerError, err.Error())
-                	return
+			response.AddHeader("Content-Type", "text/plain")
+			response.WriteErrorString(http.StatusInternalServerError, err.Error())
+			return
 		}
 		/*Connect to Controller for deploying app using dt*/
-		
-	
-        default:
+
+	default:
 		log.Printf("unknown DB type: %q", JsonParam.DbType)
 		response.AddHeader("Content-Type", "text/plain")
-                response.WriteErrorString(http.StatusInternalServerError, "Unknown DB type")
-        	return
+		response.WriteErrorString(http.StatusInternalServerError, "Unknown DB type")
+		return
 	}
 	response.WriteHeader(200)
 
 }
-
